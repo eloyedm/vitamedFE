eb6df756ff52a9ee36f4c63fd334e53f06a245b9
eloyedm <eloy.edm@gmail.com>
2017-11-11 16:31:18 -0600

    agregado login y registro conectados a api

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 		event.preventDefault();
 		$.ajax({
 			method: 'POST',
-			url: '/services/login',
+			url: 'http://vitamedbe.local/services/login',
 			data: {
-				email: $('input[name="Mail"]'),
-				password: $('input[name="Pass"]'),
+				email: $('input[name="Mail"]').val(),
+				password: $('input[name="Pass"]').val(),
 
-				nombre: $('input[name="Nom"]'),
-				apellidoP: $('input[name="aPat"]'),
-				apellidoM: $('input[name="aMat"]')
+				nombre: $('input[name="Nom"]').val(),
+				apellidoP: $('input[name="aPat"]').val(),
+				apellidoM: $('input[name="aMat"]').val()
 			}
 			,success: function(data){
 				if (data.status == 202){
@@ -22,16 +22,16 @@ $(document).ready(function(){
 			}
 		})
 	})
-	$("#logInBtn").click(function(event){
+	$("#loginBtn").click(function(event){
 		window.location.replace = "./Citas.html";
 
 		event.preventDefault();
 		$.ajax({
 			method: 'POST',
-			url: '/services/login',
+			url: 'http://vitamedbe.local/services/login',
 			data: {
-				email: $('input[name="Mail"]'),
-				password: $('input[name="Pass"]')
+				email: $('input[name="Mail"]').val(),
+				password: $('input[name="Pass"]').val()
 			}
 			,success: function(data){
 				if (data.status == 202){
@@ -48,11 +48,9 @@ $(document).ready(function(){
 		})
 		$(this).addClass("active");
 
-		$("#hide-card").fadeOut(250, () => { 
+		$("#hide-card").fadeOut(250, () => {
 			$("#hide-card").find("h3")[0].innerHTML = $(this).find("strong")[0].innerHTML;
 			$("#hide-card").fadeIn(100);
 		});
 	})
 })
-
-
