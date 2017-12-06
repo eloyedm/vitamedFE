f3144382d047114fdfbaf944f5f15f2539679783
Z_bas 01 <z_bas01@hotmail.com>
2017-12-05 21:54:44 -0600

    Ajax y select en vercitas

--- src/public/js/page_ajax/1_citas.js
+++ src/public/js/page_ajax/1_citas.js
@@ -1,5 +1,5 @@
-// var serviceHost = 'http://vitalmed.xyz:8080';
-var serviceHost = 'http://localhost:8000';
+var serviceHost = 'http://vitalmed.xyz:8080';
+
 $(document).ready(function(){
 
 	$(".calendar td").on("click", function(){
@@ -13,8 +13,33 @@ $(document).ready(function(){
 	$("#confirmarCita *[name='submit']").on("click", function(){
 
 		$("#confirmarCita").fadeOut(1000);
+
+		event.preventDefault();
+		$.ajax({
+			method: 'POST',
+			url: serviceHost+'/services/nuevacita',
+			data: {
+				email: 		$('input[name="Mail"]'			).val(),
+				password: 	$('input[name="Pass"]'			).val(),
+
+				nombre: 	$('input[name="Fecha"]'			).val(),
+				nombre: 	$('input[name="Hora"]'			).val(),
+				apellidoP: 	$('input[name="Doctor"]'		).val(),
+				apellidoM: 	$('input[name="Consultorio"]'	).val(),
+				apellidoM: 	$('input[name="numCita"]'		).val()
+			}
+			,success: function(data){
+				if (data.status == 202){
+					//window.location.replace = ""
+					alert("Se ha confirmado tu cita");
+
+					//PROBABLEMENTE
+					//window.location.href = "/recordatorios";
+				}
+			}
+		})
 	});
 
-	$('#hora').timepicker({minTime:'7:00am', maxTime:'6:30pm'});
+
 
 });
